ab8bf85fe6b4fc34df0d6473a0c954975453d26d
stvnjacobs <steven.jacobs.post@gmail.com>
2014-07-14 09:51:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+it is a life counter for games
---
+++ main.js
@@ -0,0 +1,51 @@
+    var life = {};
+
+    life.totals = [40]
+
+    window.onload = function() {
+
+    function addOne() {
+        life.totals[0] = life.totals[0] + 1
+        document.getElementById("lifeOne").innerHTML = life.totals[0];
+    };
+
+    document.getElementById("addOneLife").onclick = addOne;
+
+    function addFive() {
+        life.totals[0] = life.totals[0] + 5
+        document.getElementById("lifeOne").innerHTML = life.totals[0];
+    };
+
+    document.getElementById("addFiveLife").onclick = addFive;
+
+    function minusOne() {
+        life.totals[0] = life.totals[0] - 1
+
+        if (life.totals[0] <= 0) {
+            life.totals[0] = 0
+        }
+
+        document.getElementById("lifeOne").innerHTML = life.totals[0];
+    };
+
+    document.getElementById("minusOneLife").onclick = minusOne;
+
+    function minusFive() {
+        life.totals[0] = life.totals[0] - 5
+
+        if (life.totals[0] <= 0) {
+            life.totals[0] = 0
+        }
+
+        document.getElementById("lifeOne").innerHTML = life.totals[0];
+    };
+
+    document.getElementById("minusFiveLife").onclick = minusFive;
+
+    function reset() {
+        life.totals = [40]
+        document.getElementById("lifeOne").innerHTML = life.totals[0]
+    };
+
+    document.getElementById("resetLife").onclick = reset;
+}
